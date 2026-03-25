527c275a10883a39fdf64d7174dd0b6716c71927
Chris Benson <35149326+cwb14@users.noreply.github.com>
2026-03-25 10:18:44 -0400

    Update te_figures.py

--- bin/te_figures.py
+++ bin/te_figures.py
@@ -6,6 +6,7 @@ https://academic.oup.com/plcell/article/36/4/840/7456361 # 441 TE presences;  5,
 https://cdn.elifesciences.org/articles/15716/elife-15716-v2.pdf # 2835 non-reference TE insertions with TSDs identified in total.
 https://www.nature.com/articles/s41467-020-17874-2.pdf # 6906
 https://link.springer.com/article/10.1186/s12864-017-4103-x # 274,408
+https://academic.oup.com/nsr/article/11/6/nwae188/7687832?guestAccessKey=
 
 TE Insertion Analysis – Publication Figure Generator
 ====================================================
